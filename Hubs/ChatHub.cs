0175d5eee6d3f59df088e04261d5a905943f3d62
kunalshokeen <kunalshokeen051@gmail.com>
2024-01-03 16:21:53 +0530

    init commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            // Log connected event
+            Console.WriteLine($"Client connected: {Context.ConnectionId}");
+            Console.Write( "\n" );
+
+            await Clients.Clients(Context.ConnectionId).SendAsync("getConnectionId", Context.ConnectionId);
+            // Send a notification to the connected client
+           await  Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Server", "You are connected!");
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            // Log disconnected event
+            Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+
+            // Send a notification to all clients about the disconnection
+            await Clients.All.SendAsync("ReceiveMessage", "Server", exception != null ? $"A client has disconnected {exception.Message}." : "A client has disconnected.");
+
+            await base.OnDisconnectedAsync(exception);
+    }
+        //public async Task SendMessageToCaller(string user, string message)
+        //    => await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,54 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+    connection.invoke("OnDisconnectedAsync");
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start()
+    .then(() => {
+        console.log('Connection established');
+        document.getElementById("sendButton").disabled = false;
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
+
+connection.on('ReceiveMessage', (user, message) => {
+    const notificationArea = document.getElementById('notificationArea');
+    const notificationMessage = document.createElement('p');
+    notificationMessage.textContent = `${user}: ${message}`;
+    notificationArea.appendChild(notificationMessage)
+});
+connection.on("getConnectionId", function (id) {
+    let p = document.getElementById("conId");
+    p.innerText = `your Connection Id: ${id}`;
+});
+
+
+//connection.start().then(function () {
+//    document.getElementById("sendButton").disabled = false;
+//}).catch(function (err) {
+//    return console.error(err.toString());
+//});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+
+    //connection.invoke("SendMessageToCaller", user, message);
+   connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
